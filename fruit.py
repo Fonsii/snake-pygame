3b5f103a9cadd21e9b3254c1bf346874a3aab4a6
Fonsii <lualfonso14200174@gmail.com>
2022-07-29 16:04:49 -0600

    Render fruit

--- fruit.py
+++ fruit.py
@@ -4,18 +4,17 @@ from random import randint
 
 class Fruit:
     def __init__(self):
-        self.position = [0, 0]
-
+        self.surface = pygame.image.load('resources/fruit/apple_fruit.png').convert()
+        self.rect = self.surface.get_rect()
+        self.position = self.rect.move([0,0])
 
     def generate_position(self):
-        self.position = [randint(0, 16) * 32, randint(0, 16) * 32]
+        self.position = [randint(1, 16) * 32, randint(1, 16) * 32]
 
 
     def draw(self, screen):
-        pygame.draw.rect(screen, "Green", pygame.Rect(self.position[0], self.position[1],32,32), 2)
+        screen.blit(self.surface, self.position)
 
     
     def clear(self, screen): 
-        pygame.draw.rect(screen, "Black", pygame.Rect(self.position[0], self.position[1],32,32), 2)
-
-    
+        pygame.draw.rect(screen, "Black", pygame.Rect(self.position[0], self.position[1],32,32))
